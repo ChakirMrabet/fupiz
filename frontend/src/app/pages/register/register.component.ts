1db8347b23de07c8d2f9b7cd174d64ec6e34cfa2
Chakir Mrabet <cmrabet@innerspec.com>
2026-03-21 08:32:12 -0400

    reactive forms

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,44 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  email = '';
-  password = '';
-  confirmPassword = '';
+  registerForm: FormGroup;
   isLoading = false;
   errorMsg = '';
   successMsg = '';
 
   constructor(
+    private fb: FormBuilder,
     private auth: AuthService,
-  ) {}
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordsMatchValidator });
+  }
+
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
+
+  passwordsMatchValidator(group: AbstractControl) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
 
   onSubmit() {
-    if (this.password !== this.confirmPassword) {
-      this.errorMsg = 'Passwords do not match';
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
       return;
     }
-
     this.isLoading = true;
     this.errorMsg = '';
     this.successMsg = '';
-
-    this.auth.register({ email: this.email, password: this.password }).subscribe({
+    const { email, password } = this.registerForm.value;
+    this.auth.register({ email, password }).subscribe({
       next: (response) => {
         this.isLoading = false;
         this.successMsg = response.message;
-        this.password = '';
-        this.confirmPassword = '';
+        this.registerForm.reset();
       },
       error: (err) => {
         this.isLoading = false;
